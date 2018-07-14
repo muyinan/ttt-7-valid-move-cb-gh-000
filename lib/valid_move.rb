e8670d24d4a2b3243469c333df001db116e4fc77
Dorna Mu <dorna.mu@gmail.com>
2018-07-14 18:18:52 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 include Comparable
 def valid_move?(board, index)
-  if position_taken?(board, index) != true
+  if position_taken?(board, index) != true && index != between?(0,8)
     true
   else
     false

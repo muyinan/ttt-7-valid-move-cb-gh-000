78662b4e7acafde427203c52dcf60e6c01a82344
Dorna Mu <dorna.mu@gmail.com>
2018-07-14 18:31:25 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,6 +5,7 @@ def valid_move?(board, index)
       false
   elsif position_taken?(board, index) != true #&& (between?(0,8) == true && between(0, index == between?(0,8)))
     true
+  end
   else
     false
   end
